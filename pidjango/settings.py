243d4d047e63d1f2d92c3a302f038225773e03cb
Bradley Richardson <bdennisr@umich.edu>
2024-09-24 11:36:36 -0400

    add secret key from file

--- pidjango/settings.py
+++ pidjango/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*io)yxk)5=#vy-1vzw5qfhw4g1vzh$!9(kmm593a(z$lc__)d*'
+with open(BASE_DIR / 'secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['raspberrypi']
 
 # Application definition
 
@@ -122,6 +123,9 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
